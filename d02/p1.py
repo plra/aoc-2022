cde7a661f277e32fd100773b25e7fd6c12c1412c
Pablo Aldape <pablo.aldape@gmail.com>
2022-12-04 14:41:12 -0500

    d02

---
+++ d02/p1.py
@@ -0,0 +1,24 @@
+def round_score(opp_val, p_val):
+    round_score = p_val
+    # Outcome of RPS is uniquely determined by difference in values mod 3, where R,P,S = 1,2,3
+    diff_mod_3 = (p_val - opp_val) % 3
+    if diff_mod_3 == 1:  # P wins
+        round_score += 6
+    elif diff_mod_3 == 0:  # P draws
+        round_score += 3
+    return round_score
+
+
+total_score = 0
+RPS_VALUES = [1, 2, 3]
+CODE_VALUES = dict(zip("ABCXYZ", RPS_VALUES * 2))
+
+with open("input.txt") as f:
+    for line in f:
+        opp_code, p_code = line.strip().split()
+        opp_val, p_val = CODE_VALUES[opp_code], CODE_VALUES[p_code]
+        score = round_score(opp_val, p_val)
+        total_score += score
+
+if __name__ == "__main__":
+    print(total_score)
---
+++ d02/p2.py
@@ -0,0 +1,20 @@
+from p1 import round_score, RPS_VALUES
+
+total_score = 0
+opp_code_values = dict(zip("ABC", RPS_VALUES))
+
+with open("input.txt") as f:
+    for line in f:
+        opp_code, p_code = line.strip().split()
+        opp_val = opp_code_values[opp_code]
+        # To determine P's response, we rotate "rps" right, left or not at all if P must lose, draw
+        # or win, respectively. E.g. if p_code == "X", P loses, and R/P/S is met with S/R/P
+        # (rotate right 1).
+        rotation_index = ord(p_code) - ord("Y")
+        responses = RPS_VALUES[rotation_index:] + RPS_VALUES[:rotation_index]
+        p_val = responses[RPS_VALUES.index(opp_val)]
+        score = round_score(opp_val, p_val)
+        total_score += score
+
+if __name__ == "__main__":
+    print(total_score)
